2b15605a1535c26463ab2e10d0ab1704bfda792b
Francisco Duarte M <frduartemartinez@davidson.edu>
2023-04-12 19:36:07 -0400

    updated rec from example

--- nr/nrRec.py
+++ nr/nrRec.py
@@ -26,24 +26,45 @@ radio.printDetails()      # print basic detals of radio
 sendMessage = list("Hi..Arduino UNO")  #the message to be sent
 while len(sendMessage) < 32:    
     sendMessage.append(0)
+radio.startListening()        # Start listening the radio
 
+
+c=1
 while True:
-    start = time.time()      #start the time for checking delivery time
-    # radio.write(sendMessage)   # just write the message to radio
-    # print("Sent the message: {}".format(sendMessage))  # print a message after succesfull send
-    radio.startListening()        # Start listening the radio
-    # radio.write()
-    messageRec = []
-    radio.read(messageRec,32)
-    # char receivedMessage[32] = {0} ;   // set incmng message for 32 bytes
+    akpl_buf = [c,1, 2, 3,4,5,6,7,8,9,0,1, 2, 3,4,5,6,7,8]
+    pipe = [0]
+    while not radio.available(pipe):
+        time.sleep(10000/1000000.0)
 
-    print("Message Received: ", messageRec)
+    recv_buffer = []
+    radio.read(recv_buffer, radio.getDynamicPayloadSize())
+    print ("Received:") ,
+    print (recv_buffer)
+    c = c + 1
+    if (c&1) == 0:
+        radio.writeAckPayload(1, akpl_buf, len(akpl_buf))
+        print ("Loaded payload reply:"),
+        print (akpl_buf)
+    else:
+        print ("(No return payload)")
+
+
+# while True:
+#     start = time.time()      #start the time for checking delivery time
+#     # radio.write(sendMessage)   # just write the message to radio
+#     # print("Sent the message: {}".format(sendMessage))  # print a message after succesfull send
+#     # radio.write()
+#     messageRec = []
+#     radio.read(messageRec,32)
+#     # char receivedMessage[32] = {0} ;   // set incmng message for 32 bytes
+
+#     print("Message Received: ", messageRec)
     
-    while not radio.available(0):
-        time.sleep(1/100)
-        if time.time() - start > 2:
-            print("Timed out.")  # print errror message if radio disconnected or not functioning anymore
-            break
+#     while not radio.available(0):
+#         time.sleep(1/100)
+#         if time.time() - start > 2:
+#             print("Timed out.")  # print errror message if radio disconnected or not functioning anymore
+#             break
     
     
 
